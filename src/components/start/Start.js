348454e5dcf73d43cc1dd99540b316e08b3d972f
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-11-28 15:29:20 +0100

    UPD

--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -1,31 +1,59 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
 import "../start/Start.css";
 import Magnet from "../react-bits/Magnet";
-import Arrow from "../../../assets/svg/arow.svg";
+import Arrow from "../../assets/svg/arow.svg";
+import ElectricBorder from "../react-bits/ElectricBorder";
+import Lanyard from "../react-bits/Lanyard";
 
 const Start = () => {
   return (
     <>
-      <Container>
+      {/* OVERLAY DEL LANYARD SOPRA TUTTO */}
+      <div className="lanyard-overlay">
+        <div className="lanyard-position">
+          <Lanyard position={[0, 0, 20]} gravity={[0, -50, 0]} />
+        </div>
+      </div>
+
+      <Container fluid>
         <Row>
-          <Col xs={12}>
+          <Col xs={1}>ao</Col>
+          <Col xs={10}>
             <div className="title-container d-flex flex-column align-items-center justify-content-center min-vh-100 gap-5">
               <h1>Welcome To the Vocal Remove</h1>
               <h2>Carica qualsiasi brano</h2>
               <h2>Attendi qualche secondo</h2>
               <h2>E scarica il risultato</h2>
-              <div className="d-flex">
-                <div></div>
+              <div className="d-flex gap-5">
+                <div>
+                  <img src={Arrow} alt="" />
+                </div>
                 <Magnet
-                  padding={150}
-                  magnetStrength={1}
+                  padding={100}
+                  magnetStrength={1.5}
                   disabled={window.innerWidth < 768}
                 >
-                  <Button className="start-btn">Start</Button>
+                  <ElectricBorder
+                    color="#000000ff"
+                    speed={1}
+                    chaos={0.3}
+                    thickness={3}
+                    style={{ borderRadius: 16 }}
+                  >
+                    <Button className="start-btn">Start</Button>
+                  </ElectricBorder>
                 </Magnet>
+                <div>
+                  <img
+                    style={{ transform: "rotate(180deg)" }}
+                    src={Arrow}
+                    alt=""
+                  />
+                </div>
               </div>
             </div>
           </Col>
+          <Col xs={1}>ao</Col>
         </Row>
       </Container>
     </>
