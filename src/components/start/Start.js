61b09d27fff1c7f85a0bb695cf962391b1cb45d8
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-11-28 12:15:17 +0100

    upd

--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -1,5 +1,7 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
 import "../start/Start.css";
+import Magnet from "../react-bits/Magnet";
+import Arrow from "../../../assets/svg/arow.svg";
 
 const Start = () => {
   return (
@@ -9,10 +11,19 @@ const Start = () => {
           <Col xs={12}>
             <div className="title-container d-flex flex-column align-items-center justify-content-center min-vh-100 gap-5">
               <h1>Welcome To the Vocal Remove</h1>
-              <h1>Carica qualsiasi brano</h1>
-              <h1>Attendi qualche secondo</h1>
-              <h1>E scarica il risultato</h1>
-              <Button>Carica</Button>
+              <h2>Carica qualsiasi brano</h2>
+              <h2>Attendi qualche secondo</h2>
+              <h2>E scarica il risultato</h2>
+              <div className="d-flex">
+                <div></div>
+                <Magnet
+                  padding={150}
+                  magnetStrength={1}
+                  disabled={window.innerWidth < 768}
+                >
+                  <Button className="start-btn">Start</Button>
+                </Magnet>
+              </div>
             </div>
           </Col>
         </Row>
