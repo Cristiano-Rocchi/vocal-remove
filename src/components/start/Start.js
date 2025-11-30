7a5c34869b3ec92f93b5d04d12055330df2964dc
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2025-11-30 13:28:38 +0100

    upd

--- src/components/start/Start.js
+++ src/components/start/Start.js
@@ -5,6 +5,11 @@ import Arrow from "../../assets/svg/arow.svg";
 import ElectricBorder from "../react-bits/ElectricBorder";
 import Lanyard from "../react-bits/Lanyard";
 
+/*FLAGS */
+import itaflag from "../../assets/flags/itaflag.png";
+import engflag from "../../assets/flags/engflag.png";
+import espflag from "../../assets/flags/espflag.png";
+
 const Start = () => {
   return (
     <>
@@ -17,43 +22,63 @@ const Start = () => {
 
       <Container fluid>
         <Row>
-          <Col xs={1}>ao</Col>
-          <Col xs={10}>
+          <Col xs={2}>ao</Col>
+          <Col xs={8}>
             <div className="title-container d-flex flex-column align-items-center justify-content-center min-vh-100 gap-5">
               <h1>Welcome To the Vocal Remove</h1>
               <h2>Carica qualsiasi brano</h2>
               <h2>Attendi qualche secondo</h2>
               <h2>E scarica il risultato</h2>
-              <div className="d-flex gap-5">
-                <div>
-                  <img src={Arrow} alt="" />
-                </div>
-                <Magnet
-                  padding={100}
-                  magnetStrength={1.5}
-                  disabled={window.innerWidth < 768}
-                >
-                  <ElectricBorder
-                    color="#000000ff"
-                    speed={1}
-                    chaos={0.3}
-                    thickness={3}
-                    style={{ borderRadius: 16 }}
+              <div className="text-center">
+                <img
+                  src={Arrow}
+                  alt=""
+                  className="mb-3"
+                  style={{ transform: "rotate(90deg)" }}
+                />
+                <div className="d-flex gap-4">
+                  <div>
+                    <img src={Arrow} alt="" />
+                  </div>
+                  <Magnet
+                    padding={100}
+                    magnetStrength={1.5}
+                    disabled={window.innerWidth < 768}
                   >
-                    <Button className="start-btn">Start</Button>
-                  </ElectricBorder>
-                </Magnet>
-                <div>
-                  <img
-                    style={{ transform: "rotate(180deg)" }}
-                    src={Arrow}
-                    alt=""
-                  />
+                    <ElectricBorder
+                      color="#000000ff"
+                      speed={1}
+                      chaos={0.3}
+                      thickness={3}
+                      style={{ borderRadius: 16 }}
+                    >
+                      <Button className="start-btn">Start</Button>
+                    </ElectricBorder>
+                  </Magnet>
+                  <div>
+                    <img
+                      style={{ transform: "rotate(180deg)" }}
+                      src={Arrow}
+                      alt=""
+                    />
+                  </div>
                 </div>
+                <img
+                  src={Arrow}
+                  alt=""
+                  className="mt-3"
+                  style={{ transform: "rotate(270deg)" }}
+                />
               </div>
             </div>
           </Col>
-          <Col xs={1}>ao</Col>
+          <Col xs={2}>
+            <div className="d-flex gap-2 flag-wrapper">
+              <img className="flag" src={engflag} alt="UK Flag" />
+              <img className="flag" src={itaflag} alt="Italian Flag" />
+              <img className="flag" src={espflag} alt="Spanish Flag" />
+            </div>
+          </Col>
         </Row>
       </Container>
     </>
